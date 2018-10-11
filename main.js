60abfac4d19b160d1659ae152b6581a69897aabb
entropy <alexandrisanas@gmail.com>
2018-10-12 01:44:31 +0300

    add browserify

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-let btn = document.querySelector('.btn-confirm');
+const bcrypt = require('bcryptjs');
+
+let btn = document.querySelector('.encode-btn');
 let rawString = document.querySelector('.decoder-input').value;
 
-btn.addEventListener("click", decode, false);
+btn.addEventListener("click", encode, false);
 
-function decode (e) {
+function encode (e) {
 	e.preventDefault();
-	console.log(e);
+	let phrase = document.querySelector('.decoder-input').value;
+	console.log('phrase');
+	let salt = bcrypt.getSaltSync(10);
+	// let hash = bcrypt.hashSync()
+	// console.log(e);
+}
+
+function compare() {
+
 }
 
 
