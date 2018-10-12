57d6216be5dbb201b7a0d4f9ad10c7e9f30527be
unknown <alexandrisanas@gmail.com>
2018-10-12 08:01:35 -0700

    add browserify

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 const bcrypt = require('bcryptjs');
+const R = require('ramda');
 
 let btn = document.querySelector('.encode-btn');
 let rawString = document.querySelector('.decoder-input').value;
 
 btn.addEventListener("click", encode, false);
 
+let adder = function (x) {
+		x + 6
+	}
+
+let results = R.chain(adder, [2, 3, 5, 6, 7]);
+console.log(results)
+
 function encode (e) {
 	e.preventDefault();
 	let phrase = document.querySelector('.decoder-input').value;
-	console.log('phrase');
+	console.log(phrase);
 	let salt = bcrypt.getSaltSync(10);
 	// let hash = bcrypt.hashSync()
 	// console.log(e);
@@ -16,6 +24,6 @@ function encode (e) {
 
 function compare() {
 
-}
+} 
 
 
